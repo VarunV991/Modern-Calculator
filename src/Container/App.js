083e64a9f546db73344418fa6e22ed3bb91a63ad
VarunV991 <varunofficial99@gmail.com>
2020-05-16 20:43:00 +0530

    Multi operator expression fix

--- src/Container/App.js
+++ src/Container/App.js
@@ -43,7 +43,7 @@ class App extends Component {
       let s = eval(this.state.output)
       console.log("Reached Evaluation");
       this.setState({
-        output: (s || "0") + "",
+        output: (s) + "",
         outputDisp: true
       })
     } catch(e) {
